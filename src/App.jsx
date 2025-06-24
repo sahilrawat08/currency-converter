7748f7f4bc2b55959ed708c887fb288aaad19b43
sahil <rawatsahil1508@gmail.com>
2025-06-25 00:58:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { InputBox } from './components';
-import useCurrencyInfo from './hooks/useCurrencyinfo';
+import useCurrencyInfo from './hooks/useCurrencyInfo';
 
 function App() {
   const [amount, setAmount] = useState(0);
